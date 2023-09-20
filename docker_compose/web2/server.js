c0bd209f4b222c629cb9e76be33308f20a608440
pabloalgasparini <pabloalgasparini@gmail.com>
2023-09-20 16:59:33 -0300

    primer commit

---
+++ docker_compose/web2/server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = 80
+
+app.get('/', (req, res) =>{
+    res.send('hola')
+})
+
+
+app.listen(port,()=>{console.log(`servidor corriendo en el puerto 3000`)})
